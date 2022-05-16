2c9f37047b49145104f024256dd299c159bbbca4
Estevan Simplicio Vareiro <estevan.vareiro@sfiems.com.br>
2022-05-16 06:29:31 -0400

    task_21288 e task_21287 - Estrutura e estilização da seção das logos dos clientes

--- src/SesiFlow.Web.Portal/wwwroot/js/site.js
+++ src/SesiFlow.Web.Portal/wwwroot/js/site.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
     //----------------------SANDWICH MENU----------------------//
     const navMenu = document.getElementById('nav-menu'),
         navToggle = document.getElementById('nav-toggle'),
@@ -36,5 +37,50 @@ $(document).ready(function () {
 
     navLink.forEach(n => n.addEventListener('click', linkAction));
 
+
+    //----------------------OWL CAROUSEL----------------------//
+
+    $("#slider-services").owlCarousel({
+        loop: false,
+        responsive: {
+            0: { items: 1 },
+            480: { items: 2 },
+            1000: { items: 3 },
+            1200: { items: 4 }
+
+        },
+
+        onTranslated: callBack
+
+    });
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+
+    $('.switch-right').click(function () {
+        owl.trigger('next.owl.carousel', [400]);
+    })
+
+    $('.switch-left').click(function () {
+        owl.trigger('prev.owl.carousel', [400]);
+    })
+
+    $('.switch-left i').css('background-color', 'rgb(219, 219, 219)');
+    $('.switch-left i').css('cursor', 'default');
+
+    function callBack() {
+        if ($('.owl-carousel .owl-item').first().hasClass('active')) {
+            $('.switch-left i').css('background-color', 'rgb(219, 219, 219)');
+            $('.switch-left i').css('cursor', 'default');
+        } else if ($('.owl-carousel .owl-item').last().hasClass('active')) {
+            $('.switch-right i').css('background-color', 'rgb(219, 219, 219)');
+            $('.switch-right i').css('cursor', 'default');
+        } else {
+            $('.switch-left i').css('background-color', 'var(--first-color-alt)');
+            $('.switch-right i').css('background-color', 'var(--first-color-alt)');
+            $('.switch-right i').css('cursor', 'pointer');
+            $('.switch-left i').css('cursor', 'pointer');
+        }
+    }
 
 });

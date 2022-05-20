4e75c4277cf47e788d7fbb37d7c6d09bca118283
Estevan Simplicio Vareiro <estevan.vareiro@sfiems.com.br>
2022-05-20 11:08:20 -0400

    task_22145 - Correção no bug do carrossel de serviços mais procurados

---
+++ src/SesiFlow.Web.Portal/Controllers/CatalogController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SesiFlow.Web.Portal.Controllers
+{
+
+    [Route("produtos")]
+
+    public class CatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
